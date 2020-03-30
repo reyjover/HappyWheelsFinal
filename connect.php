c7316789d4199f1b73afd31bae52bc5460ed5ec5
Thanny <60122552+reyjover@users.noreply.github.com>
2020-03-30 18:24:35 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php 
-$connect = mysqli_connect("sql12.freemysqlhosting.net","sql12330072","2gqirbNtn2", "sql12330072") or die (mysqli_error($connect));
+$connect = mysqli_connect("sql12.freemysqlhosting.net","sql12330072","2gqirbNtn2") or die (mysqli_error($connect));
 $database = mysqli_select_db($connect,"sql12330072");
 ?>
